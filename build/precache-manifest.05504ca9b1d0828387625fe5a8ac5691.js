520f66cca2827e9657724c2ea15d8062d7f314af
JatinRanka <37477429+JatinRanka@users.noreply.github.com>
2020-06-14 18:24:15 +0530

    bug fixes

---
+++ build/precache-manifest.05504ca9b1d0828387625fe5a8ac5691.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868931990aeed953714f382bc0640a64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7d0d0703d5e923433b2",
+    "url": "/static/css/main.9bb52207.chunk.css"
+  },
+  {
+    "revision": "5dc909e43a920a467a94",
+    "url": "/static/js/2.5982bfa1.chunk.js"
+  },
+  {
+    "revision": "fcdc5e536d87d4615bb78a0d5fa80be3",
+    "url": "/static/js/2.5982bfa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d0d0703d5e923433b2",
+    "url": "/static/js/main.e6698782.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
